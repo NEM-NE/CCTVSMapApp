17b2bf2e4047207ec437058c1ee8de6b1066bb85
SUNGBIN <dlatqdlatq@mju.ac.kr>
2021-01-27 09:40:58 +0900

    [01-27] 버튼 코드 샘플 구현  (1.1.0)

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import MainView from './MainView';
--- MainView.js
+++ MainView.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { StyleSheet, Image, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Image, View, Text, TouchableOpacity, Button } from 'react-native';
 import { Icon } from 'native-base'; // 추가된 코드
 import { withNavigation, navigation } from 'react-navigation';
 import MapView, { Marker, PROVIDER_GOOGLE, MAP_TYPES } from 'react-native-maps';
 import iksonData from './ikson.json';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, backgroundColor: 'white' },
   text: {
     flex: 1,
     alignItems: 'center',
@@ -33,7 +29,10 @@ class MainScreen extends React.Component {
         longitude: 126.97894,
         latitudeDelta: 0.0322,
         longitudeDelta: 0.0321,
-      }
+      },
+      //  -1은 전체 0은 차량용 판독기만, 1은 차량용 판독기 제외
+      showVehicleReader: -1,
+      btnTitle:'차량용 판독기만 보기',
     }
   }
 
@@ -54,17 +53,46 @@ class MainScreen extends React.Component {
 
   render() {
     const new_region = this.props.navigation.getParam('region');
+    let markers;
 
-    return (
-      <MapView
-        style={{ flex: 1 }}
-        zoomEnabled={true}
-        provider={PROVIDER_GOOGLE}
-        mapType="satellite"
-        minZoomLevel={13}
-        maxZoomLevel={19}
-        region={ new_region || this.state.region}
-      >
+    //  -1은 전체 0은 차량용 판독기만, 1은 차량용 판독기 제외
+    if(this.state.showVehicleReader === 1){
+      markers = <View>
+        {iksonData.map((item) =>
+          (item.purpose !== '차량번호판독용') ?
+            <Marker
+              key={item.id}
+              title={item.id + "번 CCTV"}
+              description={item.lotAddress}
+              coordinate={{
+                latitude: parseFloat(item.latitude),
+                longitude: parseFloat(item.longitude),
+              }}
+            />
+            :
+            null
+        )}
+      </View>
+    }else if(this.state.showVehicleReader === 0){
+      markers = <View>
+        {iksonData.map((item) =>
+          (item.purpose === '차량번호판독용') ?
+            <Marker
+              key={item.id}
+              title={item.id + "번 CCTV"}
+              description={item.lotAddress}
+              coordinate={{
+                latitude: parseFloat(item.latitude),
+                longitude: parseFloat(item.longitude),
+              }}
+              pinColor={'blue'}
+            />
+            :
+            null
+        )}
+      </View>
+    }else{
+      markers = <View>
         {iksonData.map((item) =>
           (item.purpose === '차량번호판독용') ?
             <Marker
@@ -87,10 +115,37 @@ class MainScreen extends React.Component {
                 longitude: parseFloat(item.longitude),
               }}
             />
-
-
         )}
-      </MapView>
+      </View>
+    }
+
+
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={{ flex: 1 }}
+          zoomEnabled={true}
+          provider={PROVIDER_GOOGLE}
+          mapType="satellite"
+          minZoomLevel={13}
+          maxZoomLevel={19}
+          region={new_region || this.state.region}
+        >
+          {markers}
+        </MapView>
+        <View>
+          <Button
+            title={this.state.btnTitle}
+            color="#f194ff"
+            onPress={() =>
+               this.setState({
+                 showVehicleReader: (this.state.showVehicleReader === -1) ? 0 : -1,
+                 btnTitle:(this.state.btnTitle === '차량용 판독기만 보기') ? '전체 보기' : '차량용 판독기만 보기',
+                })
+            }
+          />
+        </View>
+      </View>
     );
   }
 }
